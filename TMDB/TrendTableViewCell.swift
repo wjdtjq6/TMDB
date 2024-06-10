c20657d5d3bee557cdec37f60334fee4379621da
jimmy <wjdtjq6@gmail.com>
2024-06-10 22:33:00 +0900

    done

---
+++ TMDB/TrendTableViewCell.swift
@@ -0,0 +1,163 @@
+//
+//  TrendTableViewCell.swift
+//  TMDB
+//
+//  Created by t2023-m0032 on 6/10/24.
+//
+
+import UIKit
+import SnapKit
+
+class TrendTableViewCell: UITableViewCell {
+
+    let dateLabel = UILabel()
+    let hashtagLabel = UILabel()
+    
+    let uiView = UIView()
+    let uiimageView = UIImageView()
+    let clipButton = UIButton()
+    let gradeLabel = UILabel()
+    let gradeNumberLabel = UILabel()
+    
+    let titleLabel = UILabel()
+    let castLabel = UILabel()
+    let separator = UIView()
+    let detailButton = UIButton(type: .system)
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: true)
+    }
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureHierarchy()
+        configureLayout()
+        configureUI()
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    func configureHierarchy() {
+        contentView.addSubview(dateLabel)
+        contentView.addSubview(hashtagLabel)
+        
+        contentView.addSubview(uiView)
+        contentView.addSubview(uiimageView)
+        contentView.addSubview(clipButton)
+        contentView.addSubview(gradeLabel)
+        contentView.addSubview(gradeNumberLabel)
+        
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(castLabel)
+        contentView.addSubview(separator)
+        contentView.addSubview(detailButton)
+    }
+    func configureLayout() {
+        dateLabel.snp.makeConstraints { make in
+            make.top.equalTo(contentView.safeAreaLayoutGuide).offset(15)
+            make.leading.equalTo(contentView.safeAreaLayoutGuide).offset(15)
+        }
+        hashtagLabel.snp.makeConstraints { make in
+            make.top.equalTo(dateLabel.snp_bottomMargin).offset(5)
+            make.leading.equalTo(contentView.safeAreaLayoutGuide).offset(15)
+        }
+        uiView.snp.makeConstraints { make in
+            make.top.equalTo(hashtagLabel.snp_bottomMargin).offset(20)
+            make.leading.trailing.equalTo(contentView.safeAreaLayoutGuide).inset(15)
+            make.bottom.equalTo(contentView.safeAreaLayoutGuide).inset(20)
+        }
+        uiimageView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(uiView)
+            make.bottom.equalTo(uiView.snp_bottomMargin).inset(100)
+        }
+        clipButton.snp.makeConstraints { make in
+            make.top.equalTo(uiView.snp_topMargin).offset(10)
+            make.trailing.equalTo(uiView.snp.trailing).inset(15)
+            make.width.equalTo(35)
+            make.height.equalTo(35)
+        }
+        gradeLabel.snp.makeConstraints { make in
+            make.bottom.leading.equalTo(uiimageView).inset(15)
+            make.width.equalTo(30)
+            make.height.equalTo(20)
+        }
+        gradeNumberLabel.snp.makeConstraints { make in
+            make.bottom.equalTo(uiimageView).inset(15)
+            make.leading.equalTo(gradeLabel.snp_trailingMargin).offset(8)
+            make.width.equalTo(30)
+            make.height.equalTo(20)
+        }
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(uiimageView.snp_bottomMargin).offset(25)
+            make.leading.equalTo(uiView).inset(15)
+        }
+        castLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp_bottomMargin).offset(10)
+            make.leading.trailing.equalTo(uiView).inset(15)
+        }
+        separator.snp.makeConstraints { make in
+            make.top.equalTo(castLabel.snp_bottomMargin).offset(20)
+            make.leading.equalTo(uiView).inset(15)
+            make.trailing.equalTo(uiView).inset(15)
+            make.height.equalTo(1)
+        }
+        detailButton.snp.makeConstraints { make in
+            make.top.equalTo(separator.snp_bottomMargin).offset(15)
+            make.leading.equalTo(uiView).inset(15)
+            make.trailing.equalTo(uiView).inset(15)
+        }
+    }
+    func configureUI() {
+        dateLabel.font = .boldSystemFont(ofSize: 14)
+        dateLabel.textColor = .gray
+        
+        hashtagLabel.font = .boldSystemFont(ofSize: 18)
+        
+        uiView.layer.cornerRadius = 10
+        uiView.layer.borderWidth = 1
+        uiView.layer.borderColor = UIColor.white.cgColor
+//        uiView.layer.shadowOffset = CGSize(width: 10, height: 10)
+        uiView.layer.shadowColor = UIColor.black.cgColor
+        uiView.layer.shadowOpacity = 1
+        uiView.layer.shadowRadius = 2
+        
+        //uiimageView.image = UIImage(systemName: "star")
+        //uiimageView.backgroundColor = .red
+        uiimageView.layer.cornerRadius = 10
+        uiimageView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        
+        //clipButton.setImage(UIImage(systemName: "paperclip.circle.fill"), for: .normal)
+//        clipButton.setImage(UIImage(systemName: "paperclip.circle.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 35)), for: .normal)
+        clipButton.tintColor = .white
+        
+        gradeLabel.text = "평점"
+        gradeLabel.textColor = .white
+        gradeLabel.font = .systemFont(ofSize: 12)
+        gradeLabel.textAlignment = .center
+        gradeLabel.backgroundColor = .blue
+        
+//        gradeNumberLabel.text = "3.3"
+        gradeNumberLabel.font = .systemFont(ofSize: 12)
+        gradeNumberLabel.textAlignment = .center
+        gradeNumberLabel.backgroundColor = .white
+        
+//        titleLabel.text = "Alice in Borderland"
+        titleLabel.font = .boldSystemFont(ofSize: 17)
+        
+//        castLabel.text = "qmfqkldalnsdoqwbdqi"
+        castLabel.font = .systemFont(ofSize: 14)
+        castLabel.textColor = .gray
+        
+        separator.backgroundColor = .black
+        
+        detailButton.setTitle("자세히 보기                                                                                              ", for: .normal)
+        detailButton.setImage(UIImage(systemName: "greaterthan"), for: .normal)
+        detailButton.titleLabel?.font = .boldSystemFont(ofSize: 12)
+        detailButton.tintColor = .darkGray
+        detailButton.semanticContentAttribute = .forceRightToLeft
+    }
+}
--- TMDB/ViewController.swift
+++ TMDB/ViewController.swift
@@ -6,14 +6,157 @@
 //
 
 import UIKit
+import SnapKit
+import Alamofire
+import Kingfisher
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+struct Movie_Week: Decodable {
+    let results: [Results]
+}
+struct Results: Decodable {
+    let overview: String
+    let poster_path: String
+    let title: String
+    let release_date: String
+    let vote_average: Double
+    let genre_ids: [Int]
 }
 
+//struct Datas {
+//    let overview: String
+//    let poster_path: String
+//    let title: String
+//    let release_date: String
+//    let vote_average: Double
+//}
+
+struct Genre: Decodable {
+    let genres: [GenreName]
+}
+struct GenreName: Decodable {
+    let id: Int
+    let name: String
+}
+
+class ViewController: UIViewController {
+    
+    let tableView = UITableView()
+    
+    var list: [Results] = [] {
+        
+        didSet {
+            
+            tableView.reloadData()
+            
+        }
+        
+    }
+    let url = "https://api.themoviedb.org/3/trending/movie/week?api_key=7625df5fc0f0d101f25cc3ccd6531d95"
+    
+    var list2: [GenreName] = []
+    let url2 = "https://api.themoviedb.org/3/genre/movie/list"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureHierarchy()
+        configureLayout()
+        configureUI()
+        callRequest()
+        callRequestGenre()
+    }
+    func callRequestGenre() {
+        let header: HTTPHeaders = [
+            "accept":"application/json",
+            "Authorization":"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NjI1ZGY1ZmMwZjBkMTAxZjI1Y2MzY2NkNjUzMWQ5NSIsInN1YiI6IjY2NjA2ODFiYzdjMTZiNjhhZjU3NTNiZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Heb6kxmB0LJQlXNCCMLH-fBnWnjDM-UPHxwFm2YsE7k"
+        ]
+        AF.request(url2,headers: header).responseDecodable(of: Genre.self) { response in
+            
+            switch response.result {
+            case .success(let value):
+                self.list2 = value.genres
+                print(self.list2)
+                //self.tableView.reloadData()
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    func callRequest() {
+        AF.request(url,method: .get).responseDecodable(of: Movie_Week.self) { response in
+            
+            print(response.response?.statusCode ?? 0)
+            
+            switch response.result {
+            case .success(let value):
+                self.list = value.results
+                //print(self.list)
+                self.tableView.reloadData()
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    func configureHierarchy() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.addSubview(tableView)
+        tableView.register(TrendTableViewCell.self, forCellReuseIdentifier: "TrendTableViewCell")
+        
+        tableView.separatorStyle = .none
+    }
+    func configureLayout() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "list.bullet"), style: .plain, target: self, action: #selector(leftBarButtonClicked))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "magnifyingglass"), style: .plain, target: self, action: #selector(rightBarButtonClicked))
+        
+        let appearance = UINavigationBarAppearance()
+        //appearance.configureWithOpaqueBackground()
+        //appearance.shadowImage = UIImage()
+        //navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+            make.bottom.equalTo(view)
+        }
+        tableView.rowHeight = 400
+    }
+    func configureUI() {
+        view.backgroundColor = .systemBackground
+    }
+    @objc func leftBarButtonClicked() {
+        
+    }
+    @objc func rightBarButtonClicked() {
+        
+    }
+}
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TrendTableViewCell", for: indexPath) as! TrendTableViewCell
+        cell.dateLabel.text = list[indexPath.row].release_date
+        
+        var jimmy: String = ""
+        for i in list[indexPath.row].genre_ids {
+            for j in 0...18 {
+                if i == list2[j].id {
+                    jimmy = jimmy + list2[j].name + " "
+                    print(jimmy, "222222")
+                }
+            }
+        }
+        cell.hashtagLabel.text = jimmy
+        let url = URL(string: "https://image.tmdb.org/t/p/w500"+list[indexPath.row].poster_path)
+        cell.uiimageView.kf.setImage(with: url)
+        cell.clipButton.setImage(UIImage(systemName: "paperclip.circle.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 35)), for: .normal)
+        cell.gradeNumberLabel.text = String(format: "%.1f",list[indexPath.row].vote_average)//"3.3"
+        cell.titleLabel.text = list[indexPath.row].title//"Alice in Borderland"
+        cell.castLabel.text = list[indexPath.row].overview//"qmfqkldalnsdoqwbdqi"
+        
+        return cell
+    }
+    
+    
+}
